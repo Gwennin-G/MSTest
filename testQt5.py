eab9906a0962a83b3980431d9794a834aa8bc15d
Gwennin <ggwennin@gmail.com>
2021-10-29 16:03:15 +0200

    signals

--- testQt5.py
+++ testQt5.py
@@ -1,32 +1,29 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget
+
 import sys
 
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
-
-# Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.button_is_checked = True
-
         self.setWindowTitle("My App")
 
-        self.button = QPushButton("Press Me!")
-        self.button.setCheckable(True)
-        self.button.released.connect(self.the_button_was_released)
-        self.button.setChecked(self.button_is_checked)
+        self.label = QLabel()
 
-        self.setFixedSize(QSize(400, 300))
-        self.button.setFixedSize(QSize(200, 100))
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
         # Set the central widget of the Window.
-        self.setCentralWidget(self.button)
+        self.setCentralWidget(container)
 
-    def the_button_was_released(self):
-        self.button_is_checked = self.button.isChecked()
-
-        print(self.button_is_checked)
 
 app = QApplication(sys.argv)
 
