09841e65c73febd5a67864aa2e87dea1e361ff7f
Gwennin <ggwennin@gmail.com>
2021-10-29 15:19:17 +0200

    test2

--- testQt5.py
+++ testQt5.py
@@ -9,14 +9,24 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.button_is_checked = True
+
         self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
+
+        self.button = QPushButton("Press Me!")
+        self.button.setCheckable(True)
+        self.button.released.connect(self.the_button_was_released)
+        self.button.setChecked(self.button_is_checked)
 
         self.setFixedSize(QSize(400, 300))
-        button.setFixedSize(QSize(200, 100))
+        self.button.setFixedSize(QSize(200, 100))
         # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        self.setCentralWidget(self.button)
 
+    def the_button_was_released(self):
+        self.button_is_checked = self.button.isChecked()
+
+        print(self.button_is_checked)
 
 app = QApplication(sys.argv)
 
